6ef1f471dd848ee092239f6b52f0e0c5f426a515
RMSM777 <ro.moragasm@icloud.com>
2026-08-15 12:38:17 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker - Checklist Züblin GCC-003
 // Cachea las páginas y librerías para que la app abra aunque no haya señal.
 
-const CACHE_NAME = 'qcdigital-v13'; // sube este número cuando publiques cambios importantes
+const CACHE_NAME = 'qcdigital-v14'; // sube este número cuando publiques cambios importantes
 
 const ARCHIVOS_PROPIOS = [
   './',
@@ -17,7 +17,7 @@ const ARCHIVOS_PROPIOS = [
   './ic-mi-plano-index.html',
   './cambio-turno-general.html',
   './reporte-pnc-rnc-index.html',
-  './marca.js',
+  './marca.js?v=1',
   './drive-integration.js',
   './piwii-unificado.js',
   './piwii.html',
